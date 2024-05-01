bb312805f108454aa89f8e159776c0acd751be0d
afnanramadhan <13521011@std.stei.itb.ac.id>
2024-05-01 16:21:12 +0700

    feat : shop system

---
+++ Assets/Scripts/Managers/ShopTemplate.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+
+
+namespace Nightmare
+{
+    public class ShopTemplate : MonoBehaviour
+    {
+        public TMP_Text titleText;
+        public TMP_Text descriptionText;
+        public TMP_Text priceText;
+    }
+
+}
