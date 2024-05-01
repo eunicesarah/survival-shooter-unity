a931a4ea95f34905688e5587e070755751cd08ce
afnanramadhan <13521011@std.stei.itb.ac.id>
2024-05-01 16:21:12 +0700

    feat : shop system

---
+++ Assets/Scripts/Managers/ShopManager.cs
@@ -0,0 +1,77 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Nightmare
+{
+    public class ShopManager : MonoBehaviour
+    {
+        public int coins;
+        public TMP_Text coinsUI;
+        public ShopItemSo[] shopItemSO;
+        public GameObject[] shopPanelsGO;
+        public ShopTemplate[] shopPanels;
+        public Button[] myPurchaseButton;
+        // Start is called before the first frame update
+        void Start()
+        {
+            for (int i = 0;i < shopItemSO.Length; i++)
+            {
+                shopPanelsGO[i].SetActive(true);
+            }
+
+
+            coinsUI.text = "Coins : " +coins.ToString();
+            LoadPanels();
+            CheckPurchaseable();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+
+        public void AddCoins()
+        {
+            coins++;
+            coinsUI.text = "Coins : "+ coins.ToString();
+            CheckPurchaseable();
+        }
+
+        public void CheckPurchaseable()
+        {
+            for (int i = 0; i < shopItemSO.Length; i++)
+            {
+                if (coins >= shopItemSO[i].price)
+                    myPurchaseButton[i].interactable = true;
+                else
+                    myPurchaseButton[i].interactable = false;
+            }
+        }
+
+        public void PurchaseItem(int buttonNo)
+        {
+            if (coins >= shopItemSO[buttonNo].price)
+            {
+                coins-= shopItemSO[buttonNo].price;
+                coinsUI.text = "Coins : " + coins.ToString();
+                CheckPurchaseable();
+
+            }
+        }
+
+        public void LoadPanels()
+        {
+            for (int i = 0; i < shopItemSO.Length; i++)
+            {
+                shopPanels[i].titleText.text = shopItemSO[i].title;
+                shopPanels[i].descriptionText.text = shopItemSO[i].description;
+                shopPanels[i].priceText.text = shopItemSO[i].price.ToString();
+            }
+        }
+    }
+}
