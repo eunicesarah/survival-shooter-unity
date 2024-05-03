5a25228246c810287f3c2ae15fffe20f7ae4766c
afnanramadhan <13521011@std.stei.itb.ac.id>
2024-05-03 23:07:32 +0700

    refactor coins manager from shopmanager

---
+++ Assets/Scripts/Managers/CoinsManager.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class CoinsManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public int coins;
+    public bool unlimitedCoins = false;
+    public TMP_Text coinsUI;
+
+
+    void Awake()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        coinsUI.text = "Coins : " + coins.ToString();
+    }
+
+    public void UpdateCoinsUI()
+    {
+        coinsUI.text = "Coins : " + coins.ToString();
+    }
+
+    public void AddCoins(int amount)
+    {
+        coins += amount;
+    }
+
+    public void SubstractCoins(int amount)
+    {
+        if(coins-amount>=0)
+        {
+            coins -= amount;
+        }
+
+
+    }
+}
