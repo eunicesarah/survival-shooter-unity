b5a2b02f1f4b88d286a781da77c9953a111946ee
Syarifa Purnamasari <13521018@std.stei.itb.ac.id>
2024-05-04 20:04:59 +0700

    faet: add shotgun

--- Assets/Scripts/Player/PlayerShooting.cs
+++ Assets/Scripts/Player/PlayerShooting.cs
@@ -8,11 +8,13 @@ namespace Nightmare
     public class PlayerShooting : PausibleObject
     {
         public int damagePerShot = 20;
+        public int bulletsPerTap = 3;
         public float timeBetweenBullets = 0.15f;
         public float range = 100f;
         public GameObject grenade;
         public float grenadeSpeed = 200f;
         public float grenadeFireDelay = 0.5f;
+        public float spread = 2f;
 
         float timer;
         Ray shootRay = new Ray();
@@ -22,14 +24,20 @@ namespace Nightmare
         LineRenderer gunLine;
         AudioSource gunAudio;
         Light gunLight;
+        WeaponManager weaponManager;
 		//public Light faceLight;
         float effectsDisplayTime = 0.2f;
         int grenadeStock = 99;
+        int bulletsShot;
+        bool readyToShoot = true;
+
+        public int weapon = 0;
   
         private UnityAction listener;
 
         void Awake ()
         {
+
             // Create a layer mask for the Shootable layer.
             shootableMask = LayerMask.GetMask ("Shootable", "Enemy");
 
@@ -38,6 +46,7 @@ namespace Nightmare
             gunLine = GetComponent <LineRenderer> ();
             gunAudio = GetComponent<AudioSource> ();
             gunLight = GetComponent<Light> ();
+            weaponManager = FindObjectOfType<WeaponManager>();
 			//faceLight = GetComponentInChildren<Light> ();
 
             AdjustGrenadeStock(0);
@@ -60,11 +69,12 @@ namespace Nightmare
             if (isPaused)
                 return;
 
+            weapon = weaponManager.selectedWeapon;
             // Add the time since Update was last called to the timer.
             timer += Time.deltaTime;
 
 #if !MOBILE_INPUT
-            if (timer >= timeBetweenBullets && Time.timeScale != 0)
+            if (timer >= timeBetweenBullets && Time.timeScale != 0 && readyToShoot)
             {
                 // If the Fire1 button is being press and it's time to fire...
                 if (Input.GetButton("Fire2") && grenadeStock > 0)
@@ -77,6 +87,8 @@ namespace Nightmare
                 else if (Input.GetButton("Fire1"))
                 {
                     // ... shoot the gun.
+                    bulletsShot = bulletsPerTap;
+                    readyToShoot = false;
                     Shoot();
                 }
             }
@@ -109,50 +121,69 @@ namespace Nightmare
 
         void Shoot ()
         {
+            
             // Reset the timer.
             timer = 0f;
 
             // Play the gun shot audioclip.
-            gunAudio.Play ();
+            gunAudio.Play();
 
             // Enable the lights.
             gunLight.enabled = true;
-			//faceLight.enabled = true;
+            //faceLight.enabled = true;
 
             // Stop the particles from playing if they were, then start the particles.
-            gunParticles.Stop ();
-            gunParticles.Play ();
+            gunParticles.Stop();
+            gunParticles.Play();
 
             // Enable the line renderer and set it's first position to be the end of the gun.
             gunLine.enabled = true;
-            gunLine.SetPosition (0, transform.position);
+            gunLine.SetPosition(0, transform.position);
 
             // Set the shootRay so that it starts at the end of the gun and points forward from the barrel.
             shootRay.origin = transform.position;
-            shootRay.direction = transform.forward;
+            if (weapon == 0)
+            {
+                bulletsShot = 1;
+                shootRay.direction = transform.forward;
+            }
+            if (weapon == 1)
+            {
+                float x = Random.Range(-spread, spread);
+                float y = Random.Range(-spread, spread);
+
+                shootRay.direction = transform.forward + new Vector3(x, y, y);
+            }
 
             // Perform the raycast against gameobjects on the shootable layer and if it hits something...
-            if(Physics.Raycast (shootRay, out shootHit, range, shootableMask))
+            if (Physics.Raycast(shootRay, out shootHit, range, shootableMask))
             {
                 // Try and find an EnemyHealth script on the gameobject hit.
-                EnemyHealth enemyHealth = shootHit.collider.GetComponent <EnemyHealth> ();
+                EnemyHealth enemyHealth = shootHit.collider.GetComponent<EnemyHealth>();
 
                 // If the EnemyHealth component exist...
-                if(enemyHealth != null)
+                if (enemyHealth != null)
                 {
                     // ... the enemy should take damage.
-                    enemyHealth.TakeDamage (damagePerShot, shootHit.point);
+                    enemyHealth.TakeDamage(damagePerShot, shootHit.point);
                 }
 
                 // Set the second position of the line renderer to the point the raycast hit.
-                gunLine.SetPosition (1, shootHit.point);
+                gunLine.SetPosition(1, shootHit.point);
             }
             // If the raycast didn't hit anything on the shootable layer...
             else
             {
                 // ... set the second position of the line renderer to the fullest extent of the gun's range.
-                gunLine.SetPosition (1, shootRay.origin + shootRay.direction * range);
+                gunLine.SetPosition(1, shootRay.origin + shootRay.direction * range);
             }
+            bulletsShot--;
+
+            if (bulletsShot > 0)
+                Invoke("Shoot", 0f);
+            else
+                readyToShoot = true;
+
         }
 
         private void ChangeGunLine(float midPoint)
