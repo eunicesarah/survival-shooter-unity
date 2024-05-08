ea1ddcb59bc3e842de6294a142ea29daa60f295a
afnanramadhan <13521011@std.stei.itb.ac.id>
2024-05-08 16:39:57 +0700

    feat: add cheat orbs

--- Assets/Scripts/Orbs/spawnOrbs.cs
+++ Assets/Scripts/Orbs/spawnOrbs.cs
@@ -31,4 +31,19 @@ public class SpawnOrbs : MonoBehaviour
         // Instantiate the orb at the given position
         Instantiate(orbToSpawn, position, Quaternion.identity);
     }
+
+    public void SpawnAllOrbs(Transform playerTransform)
+    {
+         float distance = 2.0f;
+        float yOffset = 0.0f; 
+        float xOffset = 1.0f;  
+
+        Vector3 forwardDirection = playerTransform.forward;
+        Vector3 rightDirection = playerTransform.right;
+        Vector3 playerPosition = playerTransform.position;
+
+        Instantiate(speedUpOrb, playerPosition + forwardDirection * distance + rightDirection * -xOffset + new Vector3(0f, yOffset, 0f), Quaternion.identity);
+        Instantiate(damageUpOrb, playerPosition + forwardDirection * distance + new Vector3(0f, yOffset, 0f), Quaternion.identity);
+        Instantiate(healthUpOrb, playerPosition + forwardDirection * distance + rightDirection * xOffset + new Vector3(0f, yOffset, 0f), Quaternion.identity);
+    }
 }
