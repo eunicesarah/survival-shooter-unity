c459892bd32570cf1dc86c39f8e5b0d6b5839612
syauqijan <13521014@std.stei.itb.ac.id>
2024-05-09 16:51:14 +0700

    fix : orbs speed

--- Assets/Scripts/Orbs/powerUpSpeed.cs
+++ Assets/Scripts/Orbs/powerUpSpeed.cs
@@ -9,13 +9,25 @@ public class powerUpSpeed : MonoBehaviour
     public PlayerMovement playerMovement;
     public float speedIncreasePercentage = 0.2f;
     public GameObject pickupEffect;
-
-
+    public void StartDestroyTimer(float delay)
+    {
+            StartCoroutine(DestroyAfterDelay(delay));
+    }
+     IEnumerator DestroyAfterDelay(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        if (gameObject != null)
+        {
+            Destroy(gameObject);
+        }
+    }
     private void Awake()
     {
         GameObject player = GameObject.FindGameObjectWithTag("Player");
         playerMovement = player.GetComponent<PlayerMovement>();
         // baseSpeed = playerMovement.speed; // Store the base speed
+        StartCoroutine(DestroyAfterDelay(5f)); // Start the destroy timer
+
     }
     private void OnTriggerEnter(Collider other)
     {
